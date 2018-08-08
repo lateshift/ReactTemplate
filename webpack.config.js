ea0dd0110acdc92a46ae770313815b224b50e7f9
Jens C <jcalisti@gmail.com>
2018-08-08 13:49:28 +0200

    Migrated to ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader'
+        loader: 'ts-loader'
       },
       {
         test: /\.less$/,
